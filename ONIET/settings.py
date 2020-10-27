9e239311cefcc2f232fb9324d738732da5fd2313
Luis Méndez <mendezgla.56@gmail.com>
2020-10-27 15:18:13 -0300

    'templates'

--- ONIET/settings.py
+++ ONIET/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ONIET.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['*'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def get_data(pais):
+    #
+    #
+    #
+    #
+
+
+def index(request):
+    user = #
+    data = get_data(user.pais)
+    context = {
+        'confirmados': get_data(),
+        'total': get_data()
+    }
+    return render(request, 'index.html', context)
+
