a89131d1892588f7c9f931ff441ba0cdeaab7055
Arel Conde <benitocondeolgado@gmail.com>
2020-10-27 16:55:03 -0300

    Lista de paises 2.0

--- stats/getCountries.py
+++ stats/getCountries.py
@@ -9,19 +9,10 @@ response = requests.request("GET", url, headers=headers, data = payload)
 
 res = response.json()
 
-#def jprint(obj):
-#    text = json.dumps(obj, sort_keys=True,indent=4)
-#    print(text)
-#jprint(res)
-
 paises = []
 
 for c in res:
     pais = c["Country"]
     paises.append(pais)
 
-print(paises)
-
-
-
-# print(response.text.encode('utf8'))
+print(sorted(paises))
